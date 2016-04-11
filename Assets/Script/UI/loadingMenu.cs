71cc898047572c97326501715010dcad3906f8ae
Deon Coke <onioncgi@gmail.com>
2016-04-11 11:24:50 -0500

    Some models have been added
    tyres
    boxs
    rockets
    character

---
+++ Assets/Script/UI/loadingMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadingMenu : MonoBehaviour {
+
+    public GameObject LoadPanel;
+    int MaxRan = 3;
+
+	// Use this for initialization
+	void Start () {
+        LoadPanel = LoadPanel.GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+	     if(Time.deltaTime >= 10)
+        {
+            ///LoadPanel.GetComponent<Image>().
+
+            SceneManager.LoadScene(3);
+        }
+	}
+}
